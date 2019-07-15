24fd02a2a8cc70308251407f8dbd719d8aa0a4eb
chenyin151 <chenyin151@163.com>
2019-07-15 16:25:16 +0800

    no message

--- js/以函数为行为单位.js
+++ js/以函数为行为单位.js
@@ -15,4 +15,17 @@ function isIndexed(data) {
 function second(a) {
    return naiveNth(a, 1) 
 }
-console.log(naiveNth(['a','b','c'],1))
+function lameCSV(str) {
+    // 把str根据换行分割成项，并把这个项存入row里面，那么三次遍历，row的值是name, age, hair
+    // Merber, 35, red 和Bob, 64, blonde
+    return _.reduce(str.split('\n'), function(table, row) {
+        console.log('reduce', table, row)
+        // _.map把数组的每一项都遍历出来，并去除空白项把这个项添加到table中
+        table.push(_.map(row.split(','),function(c){
+            return c.trim();
+        }))
+        return table
+    }, [])
+}
+var peopleTable = lameCSV('name,age,hair\nMerble,35,red\nBob, 64, blonde');
+console.log(naiveNth(['a','b','c'], 1))
